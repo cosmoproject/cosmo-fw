d9db22f056b76bd1938c616233a926909797b425
COSMO Box1 <csoundonstage@gmail.com>
2015-11-22 14:11:19 +0000

    Updated csound file paths to absolute

--- csndPython_SPI_test.py
+++ csndPython_SPI_test.py
@@ -3,10 +3,22 @@ from cosmoavr.cosmohat import CosmoHat
 from time import sleep, time
 import csnd6
 
-csoundFile = "../csound/spi-input-test.csd"
-#csoundFile = "../csound/audio-spi-input-test.csd"
-#csoundFile = "../csound/reverb_tremo.csd"
-#csoundFile = "../csound/Filter_Reverb_Delay.csd"
+#csoundFile = "/home/pi/csound/Pitchflex.csd"
+#csoundFile = "/home/pi/csound/audio-spi-input-test.csd"
+#csoundFile = "/home/pi/csound/reverb.csd"
+#csoundFile = "/home/pi/csound/delay_stereo.csd"
+#csoundFile = "/home/pi/csound/multi_delay_stereo.csd"
+#csoundFile = "/home/pi/csound/conv.csd"
+#csoundFile = "/home/pi/csound/grain.csd"
+#csoundFile = "/home/pi/csound/pvsmooth.csd"
+#csoundFile = "/home/pi/csound/pvsblur.csd"
+#csoundFile = "/home/pi/csound/pvsfreeze.csd"
+#csoundFile = "/home/pi/csound/Filter_Reverb_Delay.csd"
+#csoundFile = "/home/pi/csound/audio-in-test.csd"
+#csoundFile = "/home/pi/csound/FX4_Shutter2.csd"
+#csoundFile = "/home/pi/csound/FX5_BerntDuo_Repeater2.csd"
+#csoundFile = "/home/pi/csound/csound_conf2015.csd"
+csoundFile = "/home/pi/cosmo-dsp/WorkshopTestFiles/synthesizer-knob-test.csd"
 
 def safesleep(amt):
     if amt > 0:
@@ -19,9 +31,12 @@ def main(c):
     if res == 0:
 	perf = csnd6.CsoundPerformanceThread(cs)
 	perf.Play()
- 
+
+    switch_state = c.switches()
+    switch_last = switch_state
     last_nobs = c.nobs()
     tolerance = 0.001
+    first = True
 
     count = 0
     now = time()
@@ -31,19 +46,34 @@ def main(c):
         last_nobs = nobs
 
         for i, (nob, change) in enumerate(zip(nobs, changed)):
-            if change:
+            if change or first:
                 cs.SetChannel("P"+str(i), nob)
                 print("{}: ".format(i) + "=" * int(nob*80))
-        leds = {}
+
         switches = c.switches()
-        print(switches)
-        for i, sw in enumerate(switches):
-            leds[i] = sw != ((count+i) % 8 != 0)
+        posedge = [s and not l for s,l in zip(switches, switch_last)]
+        changed = [s != l for s,l in zip(switches, switch_last)]
+        switch_last = switches
+
+        for i, edge in enumerate(posedge):
+            if edge or first:
+                switch_state[i] = not switch_state[i]
+                cs.SetChannel("T"+str(i), switch_state[i])
+
+        for i, change in enumerate(changed):
+            if change or first:
+                cs.SetChannel("M"+str(i), switches[i])
+
+        leds = {}
+        for i in range(c.nleds):
+            leds[i] = cs.GetChannel("L"+str(i)) != 0
+            #leds[i] = switch_state[i]
         c.set_leds(leds)
         
-        now += 0.01
+        now += 0.005
         safesleep(now-time())
         count += 1
+        first = False
 
 
 
