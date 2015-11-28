665332fbad4ed5573bf08407e1c8896d2f134cb5
CosmoHat <none@example.com>
2015-11-28 02:30:18 +0000

    Make script for helping setup the hardware

---
+++ setup_hw.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from __future__ import print_function
+from cosmoavr.cosmohat import CosmoHat
+import time
+
+def main():
+    
+    c = CosmoHat()
+    print("Got version '{}'".format(c.version()))
+    j = 0;
+    while True:
+        for i, nob in enumerate(c.nobs()):
+            print("nob {}: {}".format(i, int(nob*1024*8)))
+        print("")
+        for i, sw in enumerate(c.switches()):
+            print("sw {}: {}".format(i, sw))
+        print("")
+        for i in range(c.nleds):
+            c.set_led(i, i == j)
+        j = (j + 1) % c.nleds
+        time.sleep(0.1)
+    
+    
+    
+if __name__ == "__main__":
+    main()
