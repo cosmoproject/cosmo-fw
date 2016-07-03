0dbf6b0ee75c4a14423dfe8b810d5cba0ac16be3
Kristoffer Ellersgaard Koch <kristoffer.koch@gmail.com>
2016-07-03 13:15:05 +0200

    Record range min/max

--- setup_hw.py
+++ setup_hw.py
@@ -8,9 +8,16 @@ def main():
     c = CosmoHat()
     print("Got version '{}'".format(c.version()))
     j = 0;
+
+    knobs = list(c.knobs(raw=True))
+    lowest = knobs[:]
+    highest = knobs[:]
+    
     while True:
         for i, knob in enumerate(c.knobs(raw=True)):
-            print("knob {}: {}".format(i, int(knob)))
+            lowest[i] = min(lowest[i], knob)
+            highest[i] = max(highest[i], knob)
+            print("knob {}: {:4} ({}-{})".format(i, int(knob), lowest[i], highest[i]))
         print("")
         for i, sw in enumerate(c.switches()):
             print("sw {}: {}".format(i, sw))
